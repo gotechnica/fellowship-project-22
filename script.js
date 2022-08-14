07ba73eef24ccaf599d0115949bb506b3ef7e24d
carolynl23 <ckl62015@gmail.com>
2022-08-14 10:26:03 -0400

    new commits

---
+++ script.js
@@ -0,0 +1,23 @@
+document.write("this is a text on the document written through javascript!");
+document.body.innerHTML +="<h1"
+
+*/3 primary ways to access elements on a webpage
+document.getElementById(id_name);
+document.getElemntsByClassName(class_name);
+document.querySelector();
+
+<div class="container">
+    <p id="big_pph"this is a pph!> </p>
+</div>
+
+
+let button1 = document.getElementById('btn');
+
+BigInt.addEventListener("click, function() {}
+
+
+
+
+
+    
+
